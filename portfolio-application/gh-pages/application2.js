0c9699b9d4f533d16277fed77b7b2a4e45bfdb32
Samuel Frost (PC) <samuelfurosuto@gmail.com>
2020-11-15 03:54:34 +0900

    update gh-pages

---
+++ portfolio-application/gh-pages/application2.js
@@ -0,0 +1,16 @@
+var views = {
+	welcome: {
+		index_copy: {
+			welcome: "Copy of me landing page's view matey!"
+		},
+		index: {
+			welcome: "Welcome to me app matey!"
+		}
+	}
+};
+var application = {
+	views: views
+};
+
+export default application;
+export { views };
