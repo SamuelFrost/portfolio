d6b6eae82a72963c6b7d71c6950f9960358b31ed
Samuel Frost (PC) <samuelfurosuto@gmail.com>
2021-01-20 03:35:04 +0900

    update gh-pages

--- portfolio-application/gh-pages/error_404.js
+++
@@ -1,7 +0,0 @@
-import { L as LitElement, h as html, t as translate, l as localeConfig } from './locale.js';
-
-let _=t=>t,_t;class ViewStandardError404 extends LitElement{render(){return html(_t||(_t=_`
-      <h1>${0}</h1>
-    `),translate("views.standard.error_404.main"));}}customElements.define('view-standard-error_404',ViewStandardError404);localeConfig.initialize();
-
-export { ViewStandardError404 };
--- portfolio-application/gh-pages/index.js
+++
@@ -1,7 +0,0 @@
-import { L as LitElement, h as html, t as translate, l as localeConfig } from './locale.js';
-
-let _=t=>t,_t;class ViewWelcomeIndex extends LitElement{render(){return html(_t||(_t=_`
-      <h1>${0}</h1>
-    `),translate("views.welcome.index.welcome"));}}customElements.define('view-welcome-index',ViewWelcomeIndex);localeConfig.initialize();
-
-export { ViewWelcomeIndex };
