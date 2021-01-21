4fa5510fc6383088060f72acbd6cf679a140ad1d
Samuel Frost (PC) <samuelfurosuto@gmail.com>
2021-01-21 21:29:13 +0900

    update gh-pages

---
+++ portfolio-application/gh-pages/b0b5b996.js
@@ -0,0 +1,1 @@
+import{L as e,h as r,t as i,l as s}from"./b02ea7ed.js";let a,t=e=>e;class d extends e{render(){return r(a||(a=t` <iframe style="border:0;margin:0;padding:0;display:block" src="${0}" width="100%" height="100%"> </iframe> `),i("views.resume.index.resource_url"))}}customElements.define("view-resume-index",d),s.initialize();export{d as ViewResumeIndex};
