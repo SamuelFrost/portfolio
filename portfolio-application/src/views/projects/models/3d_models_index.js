20107b249477b0cbf39f664b58bd4e841f56e81a
Samuel Frost (PC) <samuelfurosuto@gmail.com>
2021-01-24 08:38:07 +0900

    Add descriptions to models and rework some logic/structure

--- portfolio-application/src/views/projects/models/3d_models_index.js
+++ portfolio-application/src/views/projects/models/3d_models_index.js
@@ -1,48 +1,48 @@
 import { LitElement, html } from "lit-element";
 import { ModelViewerElement } from '@google/model-viewer'
-import { translate } from "lit-translate";
+import { translate} from "lit-translate";
 import { localeConfig } from "/src/locale.js";
 
 export class ViewModelIndex extends LitElement {
-  static get properties(){
+  static get properties() {
     return {
-      model_url: {type: String}
-    }
+      model_url: { type: String },
+      model_description: { type: String },
+    };
   }
-  constructor(){
+
+  constructor() {
     super();
-    this.model_url = "https://storage.googleapis.com/portfolio-media/my_balcony.glb";
+    this.model_description = this.model_info_array[0].description;
+    this.model_url = this.model_info_array[0].url;
   }
+
   render() {
     return html`
       <div
-        style="width:100%;height:100%;display:grid;grid-template-rows: min-content 1fr;"
+        style="width:100%;height:100%;display:grid;grid-template-rows: min-content fit-content(20%) 1fr;"
       >
         <div>
-          <select style="width:100%" @change=${e => this._load_model(e.target.value)}>
-            <option
-              value="https://storage.googleapis.com/portfolio-media/my_balcony.glb"
-            >
-              ${translate(
-                "views.projects.models.3d_models_index.object_title_balcony"
+          <select
+            style="width:100%"
+            @change=${(e) =>
+              this._load_model(
+                e.target.value
               )}
-            </option>
-            <option
-              value="https://storage.googleapis.com/portfolio-media/real_estate_development_project_3d_model.glb"
-            >
-              ${translate(
-                "views.projects.models.3d_models_index.object_title_house"
-              )}
-            </option>
-            <option
-              value="https://storage.googleapis.com/portfolio-media/my_balcony.glb"
-            >
-              ${translate("common.in_progress")}
-            </option>
+          >
+            ${html`${this.model_info_array.map(
+              (model_info, index) =>
+                html` <option value="${index}" title="${model_info.description}">
+                  ${model_info.name}
+                </option>`
+            )}`}
           </select>
         </div>
+        <div style="overflow:auto;">
+          ${this.model_description}
+        </div>
         <model-viewer
-          style="width:100%;height:100%;"
+          style="width:100%;height:100%;border:none;"
           src="${this.model_url}"
           camera-controls
         >
@@ -50,9 +50,28 @@ export class ViewModelIndex extends LitElement {
       </div>
     `;
   }
-  _load_model(url) {
-    this.model_url = url;
+  _load_model(index) {
+    this.model_url = this.model_info_array[index].url;
+    this.model_description = this.model_info_array[index].description;
   }
+  model_info_array = [
+    {
+      name: translate("views.projects.models.3d_models_index.object_name_house"),
+      description: translate("views.projects.models.3d_models_index.object_description_house"),
+      url: "https://storage.googleapis.com/portfolio-media/real_estate_development_project_3d_model.glb",
+    },
+    {
+      name: translate("views.projects.models.3d_models_index.object_name_microRoom"),
+      description: translate("views.projects.models.3d_models_index.object_description_microRoom"),
+      url: "https://storage.googleapis.com/portfolio-media/microRoom.glb",
+    },
+    {
+      name: translate("views.projects.models.3d_models_index.object_name_balcony"),
+      description: translate("views.projects.models.3d_models_index.object_description_balcony"),
+      url: "https://storage.googleapis.com/portfolio-media/my_balcony.glb",
+    }
+
+  ];
 }
 customElements.define("view-model-index", ViewModelIndex);
 
