9db38e1f420295437a9f25065287731fd9d61a23
Samuel Frost (PC) <samuelfurosuto@gmail.com>
2021-01-28 08:15:27 +0900

    update gh-pages

--- portfolio-application/gh-pages/2baa10c9.js
+++
@@ -1,1 +0,0 @@
-import{L as e,h as r,t as s,l as a}from"./b7a88f8c.js";let t,i=e=>e;class n extends e{render(){return r(t||(t=i` <h1>${0}</h1> `),s("views.standard.error_404.main"))}}customElements.define("view-standard-error_404",n),a.initialize();export{n as ViewStandardError404};
